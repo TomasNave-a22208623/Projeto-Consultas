efdf04b6940689b061288112b65c85289faf82e1
AndreJesus <andrejesus2003@gmail.com>
2024-11-14 11:52:51 -0500

    novo user

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         UserService userService = new UserService();
-
+            ///yasgdashdjkashfhas
         // Registrando um usuário de teste
         userService.registerUser("testuser@example.com", "testpassword");
         userService.registerUser("ola", "ola");
+        userService.registerUser("andre", "andre");
+        
     }
 }
