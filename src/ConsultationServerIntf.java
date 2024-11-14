2799f4ea28a710ef853620ca54547761a70d291b
TomasNave <tomasdasilvanave@gmail.com>
2024-11-14 18:03:57 -0500

    Intf

--- src/ConsultationServerIntf.java
+++ src/ConsultationServerIntf.java
@@ -1,8 +1,10 @@
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.*;
 import java.util.List;
-import java.rmi.Remote;
 
-public class ConsultationServerIntf extends Remote{
+public interface ConsultationServerIntf extends Remote{
+    void reservarConsulta(String clinicName, String specialty, String dateTime, int userId) throws RemoteException;
+    List<String> listarConsultas(int userID) throws RemoteException;
+    void updateConsulta(int consultationId, String newClinicName, String newSpecialty, String newDateTime) throws RemoteException;
+    void cancelarConsulta(int consultationId) throws RemoteException;
 
 }
