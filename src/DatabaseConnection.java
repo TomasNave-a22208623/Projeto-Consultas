feb7fff5118cb7158c5c58493a1a93b7e052b282
TomasNave <tomasdasilvanave@gmail.com>
2024-11-15 14:48:36 -0500

    problema rmi resolvido

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -8,12 +8,21 @@ public class DatabaseConnection {
     private static final String PASSWORD = "tominho123";
 
     public static Connection getConnection() throws SQLException {
+        
         try {
             
-            Class.forName("com.mysql.cj.jdbc.Driver");  
-            return DriverManager.getConnection(URL, USER, PASSWORD);  
+            Class.forName("com.mysql.jdbc.Driver");   
          } catch (ClassNotFoundException e) {
             throw new SQLException("MySQL JDBC Driver not found", e);  
          }
+        
+        
+        try {  
+            return DriverManager.getConnection(URL, USER, PASSWORD);  
+         } catch (SQLException e) {
+            throw new SQLException("Connecção com a base de dados falhou", e);  
+         }
+
+
     }
 }
