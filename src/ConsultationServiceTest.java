ff0594cf809cc280f0d8df32bdcf336ea90b3937
TomasNave <tomasdasilvanave@gmail.com>
2024-11-16 14:50:27 -0500

    update Test

--- src/ConsultationServiceTest.java
+++ src/ConsultationServiceTest.java
@@ -54,6 +54,49 @@ public class ConsultationServiceTest {
 
 
 
+    @Test 
+    public void UpdateConsulta(){
+
+        // ------------------------- Teste Hora Invalida --------------------------------------------------
+        String resultHoraInv = ConsultationService.reservarConsulta("A", "Cardiologia", "2024-11-16 07:00", 1);
+        assertEquals("A clinica fornecido nao existe",resultHoraInv);
+
+
+
+        // ------------------------- Teste Minutos Invalidos --------------------------------------------------
+        String resultMinutosInv = ConsultationService.reservarConsulta("A", "Cardiologia", "2024-11-16 10:32", 1);
+        assertEquals("A clinica fornecido nao existe",resultMinutosInv);
+
+
+
+        // ------------------------- Teste Clinica Invalida --------------------------------------------------
+        String resultClinicaInv = ConsultationService.reservarConsulta("H", "Cardiologia", "2024-11-16 10:00", 1);
+        assertEquals("A clinica fornecido nao existe",resultClinicaInv);
+
+
+        // ------------------------- Teste especialidade Invalida --------------------------------------------------
+        String resultEspInv = ConsultationService.reservarConsulta("A", "dibil", "2024-11-16 10:00", 1);
+        assertEquals("A clinica fornecido nao existe", resultEspInv);
+
+
+        // ------------------------- Teste especialidade que nao existe na clinica --------------------------------------------------
+        String resultEspCliniInv = ConsultationService.reservarConsulta("C", "Ginecologia", "2024-11-16 10:00", 1);
+        assertEquals("A clinica fornecido nao existe",resultEspCliniInv);
+
+
+
+        
+        // ------------------------- Teste Medicos não disponivies naquele horario --------------------------------------------------
+        ConsultationService.reservarConsulta("C", "Ginecologia", "2024-11-16 10:00", 1);
+        
+        
+        String resultMedicosInds = ConsultationService.reservarConsulta("C", "Ginecologia", "2024-11-16 10:00", 1);
+        assertEquals("A clinica fornecido nao existe",resultMedicosInds);
+
+
+
+    }
+
 
 
 
